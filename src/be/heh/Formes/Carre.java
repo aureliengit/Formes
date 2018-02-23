cd294b4db7a11d39e7dd17ca50b1dfd92b28406a
aureliengit <36195349+aureliengit@users.noreply.github.com>
2018-02-23 09:05:16 +0100

    Update Carre.java

--- src/be/heh/Formes/Carre.java
+++ src/be/heh/Formes/Carre.java
@@ -3,7 +3,10 @@ package be.heh.Formes;
 public class Carre extends Forme{
 
     public Carre(float cote) {
-        super(cote, 4);
+        super(cote, 4);  //va permettre d'appeler le constructeur de parent 
+        /*question d'examen pour rappeler la methode du parent super.<nom de methode> 
+        @ est une annotation (pour verifier qu'il existe la meme methode dans la classe parent pour la signature */
+        
     }
 
     @Override
